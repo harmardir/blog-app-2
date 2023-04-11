0719ca0c3b433b3103dee7dd88ec2a5121990ea4
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-04-11 13:52:04 +0300

    creating post detail view

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from core.views import frontpage , about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage'),
     path('about/', about, name='about'),
+    path('', include('blog.urls')),
+    path('', frontpage, name='frontpage'),
+   
 ]
