52458fbed5485d2e36f753c3c6e0147889d1d92f
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-04-24 16:07:15 +0300

    adding category

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,29 @@ from django.utils.translation import gettext_lazy as _
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
+class Category(models.Model):
+    title = models.CharField(max_length= 255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
+    category = models.ForeignKey(Category , name = 'posts', on_delete=models.CASCADE)
     title = models.CharField(max_length= 255)
     slug = models.SlugField()
     intro = models.TextField()
     #body = models.TextField()
     body = RichTextUploadingField() # CKEditor Rich Text Field
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post , related_name='comments',on_delete=models.CASCADE)
@@ -19,6 +34,6 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Meta:
-    ordering = ('-created_at',)
+    class Meta:
+        ordering = ('-created_at',)
 
